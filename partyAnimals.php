ea61a6f818f1b82475c38d38d765d559043590b0
Chen-Elian <114715689+Chen-Elian@users.noreply.github.com>
2023-12-29 21:58:05 +0800

    Add files via upload

---
+++ partyAnimals.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Party Animals</title>
+
+    <link rel="stylesheet" type="text/css" href="game.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+    <nav>
+        <!-- 左邊的連結 -->
+        <a href="home.php" style="float: left;">首頁</a>
+
+        <!-- 右邊的連結 -->
+        <a href="register.php" style="float: right;">註冊</a>
+        <a href="login.php" style="float: right;">登入</a>
+    </nav>
+
+    <p>&nbsp;</p>
+    <br>
+
+    <div class="gameContent1">
+        <div class="gameImg">
+            <img src="https://chen-elian.github.io/databaseReport/Party Animals.jpg" alt="Party Animals">
+            <br>
+        </div>
+
+        <div class="gameText1">
+            <p class="gameTitle">Party Animals</p>
+            <hr>
+            <span style="color: #F1C6BF;">售價：</span>
+            <span>$419</span>
+            <p></p>
+            <span style="color: #F1C6BF;">開發商：</span>
+            <span>Source Technology</span>
+            <p></p>
+            <span style="color: #F1C6BF;">發售日期：</span>
+            <span>2023/09/20</span>
+
+        </div>
+
+    </div>
+
+    <div class="gameContent2">
+        <div class="gameText2">
+            <span style="color: #B9D489;">遊戲簡介：</span>
+            <br>這是一款基於物理的多人派對遊戲。選擇你喜歡的小動物角色，和朋友們一起挑戰其他玩家，或者
+            <br>展開激烈的對決吧！看到毛絨絨的小動物們你還不心動嗎
+            <p></p>
+            <span style="color: #B9D489;">類型：</span>
+            <br>休閒、搞笑、動作、格鬥、可愛、派對
+            <p></p>
+            <span style="color: #B9D489;">支援語言：</span>
+            <br>繁體中文、簡體中文、英文、法文、德文、西班牙文、日文、韓文、葡萄牙文、泰文
+        </div>
+    </div>
+</body>
+
+</html>
